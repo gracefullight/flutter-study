bb52b145873c369b9fd7f4fd1326b7db22d48817
Eunkwang Shin <11773683+gracefullight@users.noreply.github.com>
2020-12-06 19:04:47 +0900

    feat(basic): add appear

---
+++ basic/06-appbar.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => new _State();
+}
+
+class _State extends State<MyApp> {
+  int _value = 0;
+
+  void _add() => setState(() => _value++);
+  void _remove() => setState(() => _value--);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Hello world'),
+          backgroundColor: Colors.red,
+          actions: <Widget>[
+            new IconButton(icon: new Icon(Icons.add), onPressed: _add),
+            new IconButton(icon: new Icon(Icons.remove), onPressed: _remove)
+          ],
+        ),
+        body: new Container(
+            padding: new EdgeInsets.all(32.0),
+            child: new Center(
+              child: new Column(children: <Widget>[
+                new Text(_value.toString(),
+                    style: new TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 37.0)),
+              ]),
+            )));
+  }
+}
