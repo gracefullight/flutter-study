d787792c6d0969176523accebd2e548c7a3140f0
Eunkwang Shin <11773683+gracefullight@users.noreply.github.com>
2020-12-06 18:21:54 +0900

    feat(basic): add switch

---
+++ basic/03-switch.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => new _State();
+}
+
+class _State extends State<MyApp> {
+  bool _value1 = false;
+  bool _value2 = false;
+
+  void _onChanged1(bool value) => setState(() => _value1 = value);
+  void _onChanged2(bool value) => setState(() => _value2 = value);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Checkbox'),
+        ),
+        body: new Container(
+            padding: new EdgeInsets.all(32.0),
+            child: new Center(
+              child: new Column(children: <Widget>[
+                new Switch(value: _value1, onChanged: _onChanged1),
+                new SwitchListTile(
+                  value: _value2,
+                  onChanged: _onChanged2,
+                  title: new Text('Hello World',
+                      style: new TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.red)),
+                )
+              ]),
+            )));
+  }
+}
