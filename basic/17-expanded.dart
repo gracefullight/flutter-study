8043cf1d61675938f5af9df72d81665e01c913f5
Eunkwang Shin <11773683+gracefullight@users.noreply.github.com>
2020-12-08 00:55:05 +0900

    feat(basic): add expanded

---
+++ basic/17-expanded.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello world'),
+      ),
+      body: Container(
+          padding: EdgeInsets.all(32.0),
+          child: Center(
+            child: Column(children: <Widget>[
+              Text('Image Demo'),
+              Expanded(child: SvgPicture.asset('images/flutter.svg')),
+              Expanded(child: Image.network('https://i.imgur.com/4fWZN.jpeg'))
+            ]),
+          )),
+    );
+  }
+}
