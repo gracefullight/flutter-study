6723d05e354d0d2704e7e1c1e09d4052822b4d75
Eunkwang Shin <11773683+gracefullight@users.noreply.github.com>
2020-12-18 01:06:38 +0900

    fix(inter): add custom widget

---
+++ src/intermediate/11-custom-widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'mywidget.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Hello world'),
+        ),
+        body: Container(
+            padding: EdgeInsets.all(32.0),
+            child: Center(
+              child: Column(
+                children: <Widget>[Text('Name here'), MyWidget()],
+              ),
+            )));
+  }
+}
